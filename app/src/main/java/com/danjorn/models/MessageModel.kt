f5272398c6d331ec7080b0fe6f62e0134af9bbee
DanielJorn <0954264@gmail.com>
2019-10-04 10:07:16 +0300

    Added map function in MessageModel.

--- app/src/main/java/com/danjorn/models/MessageModel.kt
+++ app/src/main/java/com/danjorn/models/MessageModel.kt
@@ -4,14 +4,24 @@ import com.google.firebase.database.Exclude
 
 data class MessageResponse(
         @Exclude
-        override var id: String?,
+        override var id: String? = null,
         @Exclude
         var chatId: String? = null,
         @Exclude
         var authorId: String? = null,
         var messageText: String? = null,
         var timestamp: Long? = null
-) : WithId()
+) : WithId() {
+    fun toUIMessage(): Message {
+        return Message(
+                id = id,
+                chatId = chatId,
+                messageText = messageText.orEmpty(),
+                authorUser = User(id = authorId),
+                timestamp = timestamp!!
+        )
+    }
+}
 
 data class Message(
         override var id: String? = null,
